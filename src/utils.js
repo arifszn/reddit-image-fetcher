96ce044fb9484f5a7630bedcb1412873d79c39f7
Ariful Alam <swazan.arif@gmail.com>
2020-11-20 23:45:40 +0600

    added upvote and downvote info

--- src/utils.js
+++ src/utils.js
@@ -50,16 +50,19 @@ const getRequest = async (url) => {
  */
 const formatPost = (post, type) => {
     return {
-        id        : typeof post.id !== 'undefined' ? post.id : null,
-        type      : type,
-        title     : typeof post.title !== 'undefined' ? post.title : null,
-        postLink  : typeof post.id !== 'undefined' ?  'https://redd.it/' + post.id : null,
-        image     : typeof post.url !== 'undefined' ? post.url : null,
-        thumbnail : typeof post.thumbnail !== 'undefined' ? post.thumbnail : null,
-        subreddit : typeof post.subreddit !== 'undefined' ? post.subreddit : null,
-        NSFW      : typeof post.over_18 !== 'undefined' ? post.over_18 : null,
-        spoiler   : typeof post.spoiler !== 'undefined' ? post.spoiler : null,
-        createdUtc: typeof post.created_utc !== 'undefined' ? post.created_utc : null
+        id              : typeof post.id !== 'undefined' ? post.id : null,
+        type            : type,
+        title           : typeof post.title !== 'undefined' ? post.title : null,
+        postLink        : typeof post.id !== 'undefined' ?  'https://redd.it/' + post.id : null,
+        image           : typeof post.url !== 'undefined' ? post.url : null,
+        thumbnail       : typeof post.thumbnail !== 'undefined' ? post.thumbnail : null,
+        subreddit       : typeof post.subreddit !== 'undefined' ? post.subreddit : null,
+        NSFW            : typeof post.over_18 !== 'undefined' ? post.over_18 : null,
+        spoiler         : typeof post.spoiler !== 'undefined' ? post.spoiler : null,
+        createdUtc      : typeof post.created_utc !== 'undefined' ? post.created_utc : null,
+        upvotes         : typeof post.ups !== 'undefined' ? post.ups : null,
+        downvotes       : typeof post.downs !== 'undefined' ? post.downs : null,
+        upvoteRatio     : typeof post.upvote_ratio !== 'undefined' ? post.upvote_ratio : null,
     };
 }
 
